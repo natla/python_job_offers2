7d64a8530d170cb533db2447e48c2c4e2b21ea15
natla <natalia.malkovska@gmail.com>
2018-03-05 19:35:02 +0200

    Static files changes

--- scraper/settings.py
+++ scraper/settings.py
@@ -127,6 +127,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_DIR, 'static'),
+    "C:/Users/Natalie/Documents/scraper/web_scraper/static",
 )
 
 # List of finder classes that know how to find static files in
