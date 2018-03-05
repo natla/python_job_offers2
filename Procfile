8e4263e4f97c37f952846b7fda6251cf22adec2d
natla <natalia.malkovska@gmail.com>
2018-03-05 18:56:06 +0200

    Static files changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python scraper.manage collectstatic --noinput; gunicorn --workers=4 --bind=0.0.0.0:$PORT scraper.settings
+web: python manage.py collectstatic --noinput; gunicorn --workers=4 --bind=0.0.0.0:$PORT scraper/settings.py 
--- scraper/settings.py
+++ scraper/settings.py
@@ -117,14 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
     "C:/Users/Natalie/Documents/scraper/web_scraper/static",
 )
 
