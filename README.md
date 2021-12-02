4d3a824b7363cf07c44192b215af4a4bb3ee396a
Natalia Malkovska <natalia.malkovska@gmail.com>
2021-12-02 15:37:52 +0200

    Refactor scraping.py and fix the templates
    Additionally, fix the static files location in the settings and
    the typos in the README file.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# python_job_offers2
-List curent job openings for Python developers in Sofia, Bulgaira
+# Python Job Offers
+List current job openings for Python developers in Sofia, Bulgaria
--- web_scraper/scraping.py
+++ web_scraper/scraping.py
@@ -1,48 +1,53 @@
+from typing import Dict, List, Set, Tuple
+
 import requests
 from bs4 import BeautifulSoup as Soup
 
-print()
+HOST = 'https://www.jobs.bg/'
+PATH = 'front_job_search.php'
 
 
-# we use class_ bec class is a keyword in Python
-def get_results_on_page(scraped_url):
+def get_results_on_page(scraped_url: str) -> Dict[str, Set[Tuple[str, str]]]:
+    """Return a dictionary where the keys are Python job titles, and the values
+    are sets ot tuples containing company names and links to the job listings."""
     soup = Soup(scraped_url, 'lxml')
-    results_list = []
     results_dict = {}
 
     for search_result_row in soup.find_all('tr'):
-        job_link = search_result_row.find('a', class_="joblink")
-        company = search_result_row.find('a', class_="company_link")
-        if job_link and "Python" in job_link.text:
-            if job_link not in results_list:
-                results_list.append(job_link)
-                job_description = job_link.text
-                company_description = company.text
-                link = job_link['href']
-                if job_description not in results_dict:
-                    results_dict[job_description] = company_description, "https://www.jobs.bg/" + link
-                else:
-                    results_dict[job_description] += company_description, "https://www.jobs.bg/" + link
-                #print(job_description)
-                #print("Company: " + company_description)
-                #print("Go to offer: https://www.jobs.bg/" + link)
-                #print()
-    #print(results_dict)
+
+        job_link = search_result_row.find('a', class_='card__title')
+        company_link = search_result_row.find('a', class_='company_link')
+        if job_link and 'Python' in job_link.text:
+            job_description = job_link.text
+            company_description = company_link.text
+            link = job_link['href']
+            if job_description not in results_dict:
+                results_dict[job_description] = {(company_description, HOST + link)}
+            else:
+                results_dict[job_description].add((company_description, HOST + link))
+
     return results_dict
 
 
-def print_jobs():
-    try:
-        scraped_url1 = requests.get(
-            "https://www.jobs.bg/front_job_search.php?&location_sid=1&keywords%5B0%5D=python").text
-        scraped_url2 = requests.get(
-            "https://www.jobs.bg/front_job_search.php?frompage=15&location_sid=1&keywords%5B0%5D=python").text
-        scraped_url3 = requests.get(
-            "https://www.jobs.bg/front_job_search.php?frompage=30&location_sid=1&keywords%5B0%5D=python").text
+def print_jobs(limit: int = None) -> List[dict]:
+    """Construct urls for scrapping in the range of the limit parameter,
+    get the results on the page and return them as a list of dictionaries.
+    """
+    if limit is None:
+        limit = 3
+    results = []
 
-        return get_results_on_page(scraped_url1), get_results_on_page(scraped_url2), get_results_on_page(scraped_url3)
+    for i in range(limit):
+        scraped_url = f'{HOST}{PATH}?frompage={i * 15}&location_sid=1&keywords%5B0%5D=python'
+        try:
+            request = requests.get(scraped_url).text
+            result = get_results_on_page(request)
+            if result:
+                results.append(result)
+        except ImportError as e:
+            print("Page couldn't load properly: ", e)
 
-    except ImportError as e:
-        print("Pages couldn't load properly. Sorry for the inconvenience: ", e)
+    return results
+
 
 print_jobs()
